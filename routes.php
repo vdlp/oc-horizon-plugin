1286fa232a4a2de994fcfb793cbee79e416caee7
Alwin Drenth <adrenth@gmail.com>
2021-10-18 12:54:10 +0200

    Add fallback route if assets are not present

--- routes.php
+++ routes.php
@@ -3,18 +3,32 @@
 declare(strict_types=1);
 
 use Illuminate\Routing\Router;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Vdlp\Horizon\Classes\PathHelper;
 
 /** @var Router $router */
 $router = resolve(Router::class);
 
-$router->group(['middleware' => ['web']], static function () use ($router) {
-    $router->get('/vendor/horizon/img/horizon.svg', static function () {
-        /** @var PathHelper $helper */
-        $helper = resolve(PathHelper::class);
+/** @var PathHelper $pathHelper */
+$pathHelper = resolve(PathHelper::class);
 
-        return response()->download($helper->getAssetsPath('img/horizon.svg'), 'horizon.svg', [
+$router->group(['middleware' => ['web']], static function () use ($router, $pathHelper): void {
+    $router->get('/vendor/horizon/img/horizon.svg', static function () use ($pathHelper): BinaryFileResponse {
+        return response()->download($pathHelper->getAssetsPath('img/horizon.svg'), 'horizon.svg', [
             'Content-Type' => 'image/svg+xml',
         ]);
     });
 });
+
+if (!file_exists($pathHelper->getAssetsPath('mix-manifest.json'))) {
+    $router->group([
+        'domain' => config('horizon.domain'),
+        'prefix' => config('horizon.path'),
+        'middleware' => config('horizon.middleware', 'web'),
+    ], function () use ($router): void {
+        $router->get('/{dashboard?}', function (): string {
+            return 'The published Horizon assets are not up-to-date with the installed version. '
+                . 'To update, run:<br/><code>php artisan horizon:install</code>';
+        });
+    });
+}
