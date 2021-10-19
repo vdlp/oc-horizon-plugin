c4f25798a8678c35019bf9be33f4e4d5c1b7895d
Alwin Drenth <adrenth@gmail.com>
2021-10-19 19:28:17 +0200

    Override all Horizon routes when assets invalid

--- routes.php
+++ routes.php
@@ -2,8 +2,8 @@
 
 declare(strict_types=1);
 
+use Illuminate\Contracts\Routing\ResponseFactory;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 use Illuminate\Routing\Router;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Vdlp\Horizon\Classes\PathHelper;
@@ -16,7 +16,10 @@ $pathHelper = resolve(PathHelper::class);
 
 $router->group(['middleware' => ['web']], static function () use ($router, $pathHelper): void {
     $router->get('/vendor/horizon/img/horizon.svg', static function () use ($pathHelper): BinaryFileResponse {
-        return response()->download($pathHelper->getAssetsPath('img/horizon.svg'), 'horizon.svg', [
+        /** @var ResponseFactory $factory */
+        $factory = resolve(ResponseFactory::class);
+
+        return $factory->download($pathHelper->getAssetsPath('img/horizon.svg'), 'horizon.svg', [
             'Content-Type' => 'image/svg+xml',
         ]);
     });
@@ -28,13 +31,13 @@ if (!$pathHelper->assetsAreCurrent()) {
         'prefix' => config('horizon.path'),
         'middleware' => config('horizon.middleware', 'web'),
     ], static function () use ($router): void {
-        $router->get('/{dashboard?}', static function (Request $request) {
+        $router->get('/{anything?}', static function (Request $request): string {
             if (Laravel\Horizon\Horizon::check($request)) {
                 return 'The published Horizon assets are not up-to-date with the installed version. '
                     . 'To update, run:<br/><code>php artisan horizon:assets</code>';
             }
 
-            return new Response('Not Found', 404);
-        });
+            abort(403);
+        })->where('anything', '(.*)');
     });
 }
