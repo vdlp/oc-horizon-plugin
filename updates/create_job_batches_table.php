d90b37dfdd8523c4a9e8c2d88bf3871f1c00b121
Bryan Smit <bryan@vdlp.nl>
2022-09-13 17:25:05 +0200

    LMSD-172 Add job batches table

---
+++ updates/create_job_batches_table.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Vdlp\Horizon\Updates;
+
+use Illuminate\Database\Schema\Blueprint;
+use October\Rain\Database\Updates\Migration;
+use Schema;
+
+final class CreateJobBatchesTable extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('job_batches', static function (Blueprint $table): void {
+            $table->string('id')->primary();
+            $table->string('name');
+            $table->integer('total_jobs');
+            $table->integer('pending_jobs');
+            $table->integer('failed_jobs');
+            $table->text('failed_job_ids');
+            $table->mediumText('options')->nullable();
+            $table->integer('cancelled_at')->nullable();
+            $table->integer('created_at');
+            $table->integer('finished_at')->nullable();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::drop('job_batches');
+    }
+}
